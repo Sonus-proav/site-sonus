b15cb21a579d53ef4a3e37bdfb67206edbeb873b
Desenvolvedor Sonus <sonusproaudio@gmail.com>
2026-06-17 16:06:26 -0300

    Fix: Adiciona imagem da Matriz de Pato Branco

--- src/pages/IgrejasTemplos.tsx
+++ src/pages/IgrejasTemplos.tsx
@@ -136,7 +136,7 @@ export function IgrejasTemplos() {
     {
       name: "Matriz de Pato Branco",
       state: "PR",
-      image: "/matriz itapejara do oeste.jpg",
+      image: "/pato-branco.jpg",
       description: "Implementação de tecnologia de processamento digital, eliminando microfonias indesejadas e facilitando o controle diário do áudio para a equipe de voluntários durante os cultos e louvores."
     }
   ]
