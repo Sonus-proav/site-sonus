fbd7216263d56bf353911de2b0bd80a46b00c406
Desenvolvedor Sonus <sonusproaudio@gmail.com>
2026-08-20 22:40:30 -0300

    perf: otimiza orbs de fundo com gradiente radial e contain css para corrigir lag na GPU

--- src/main.tsx
+++ src/main.tsx
@@ -29,8 +29,8 @@ getProjects().then(projects => {
   }
 }).catch(() => {});
 
-// Cache bust 2026-08-20 v14
-console.log("App loaded v1.0.14");
+// Cache bust 2026-08-20 v15
+console.log("App loaded v1.0.15");
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
